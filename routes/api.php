1af4fd8d3e7c7ee612ae7ddff3f24b29027f625a
Developer SMKN2 <developer@smkn2sumbawa.sch.id>
2026-07-13 18:28:44 +0800

    chore: remove whatsapp-web.js sidecar, package, config, migrations

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\ApiDashboardController;
-use App\Http\Controllers\WhatsappSettingController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->get('/dashboard/stats', [ApiDashboardController::class, 'stats']);
