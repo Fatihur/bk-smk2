34db47a92f73c9cddd96655637401b9ba0f05d13
Developer SMKN2 <developer@smkn2sumbawa.sch.id>
2026-07-17 20:55:40 +0800

    fix: table layout kop surat, text center di dokumen, hapus duplikat CSS

--- resources/views/pdf/surat-teguran.blade.php
+++ resources/views/pdf/surat-teguran.blade.php
@@ -4,10 +4,11 @@
     <meta charset="utf-8">
     <title>Surat Teguran {{ $tingkat }}</title>
     <style>
-        body { font-family: 'Times New Roman', Times, serif; font-size: 12pt; line-height: 1.4; color: #000; margin: 0; padding: 25px 30px; }
-        .header { display: flex; align-items: center; margin-bottom: 5px; }
-        .header .logo { width: 90px; height: auto; margin-right: 12px; margin-top: 25px; }
-        .header .text { flex: 1; text-align: center; }
+        body { font-family: 'Times New Roman', Times, serif; font-size: 12pt; line-height: 1.4; color: #000; margin: 0; padding: 25px 10px; }
+        .header { border-collapse: collapse; width: 100%; margin-bottom: 5px; }
+        .header .logo { width: 90px; height: auto; }
+        .header .text-wrap { margin-left: -51px; }
+        .header .text { text-align: center; }
         .header .text h1 { font-size: 11pt; font-weight: normal; margin: 0; text-transform: uppercase; }
         .header .text h2 { font-size: 11pt; font-weight: normal; margin: 0; text-transform: uppercase; }
         .header .text h3 { font-size: 13pt; font-weight: bold; margin: 2px 0; text-transform: uppercase; }
@@ -22,17 +23,25 @@
     </style>
 </head>
 <body>
-    <div class="header">
-        <img src="data:image/jpg;base64,{{ base64_encode(file_get_contents(public_path('images/logo.jpg'))) }}" class="logo" alt="Logo">
-        <div class="text">
-            <h1>PEMERINTAH PROVINSI NUSA TENGGARA BARAT</h1>
-            <h2>DINAS PENDIDIKAN DAN KEBUDAYAAN</h2>
-            <h3>SMK NEGERI 2 SUMBAWA BESAR</h3>
-            <p>Jl. Lingkar Selatan Km. 04 Sumbawa Besar, Telp./Fax.: (0371) 2628048 / 2628047</p>
-            <p>Situs Resmi: http://www.smkn2sumbawabesar.sch.id</p>
-            <p>Email: smkn2_sumbawabesar@yahoo.co.id</p>
-        </div>
-    </div>
+    <table class="header">
+        <tr>
+            <td style="width:102px;vertical-align:middle;padding:0">
+                <img src="data:image/jpg;base64,{{ base64_encode(file_get_contents(public_path('images/logo.jpg'))) }}" class="logo" alt="Logo">
+            </td>
+            <td style="text-align:center;vertical-align:middle;padding:0">
+                <div class="text-wrap">
+                    <div class="text">
+                        <h1>PEMERINTAH PROVINSI NUSA TENGGARA BARAT</h1>
+                        <h2>DINAS PENDIDIKAN DAN KEBUDAYAAN</h2>
+                        <h3>SMK NEGERI 2 SUMBAWA BESAR</h3>
+                        <p>Jl. Lingkar Selatan Km. 04 Sumbawa Besar, Telp./Fax.: (0371) 2628048 / 2628047</p>
+                        <p>Situs Resmi: http://www.smkn2sumbawabesar.sch.id</p>
+                        <p>Email: smkn2_sumbawabesar@yahoo.co.id</p>
+                    </div>
+                </div>
+            </td>
+        </tr>
+    </table>
     <div class="garis"></div>
 
     <div class="title">SURAT TEGURAN {{ $tingkat }}</div>
